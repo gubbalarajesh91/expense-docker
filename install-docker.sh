a43a44d4fbeb12b6cb65e8390e4b6f46ee2ee1fb
gubbalarajesh91 <gubbala.rajesh91@gmail.com>
2024-06-24 22:36:58 +0530

    dcoker-expense

--- install-docker.sh
+++ install-docker.sh
@@ -5,3 +5,5 @@ yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 usermod -aG docker ec2-user
+
+#curl https://raw.githubusercontent.com/gubbalarajesh91/expense-docker/main/install-docker.sh | sudo bash
